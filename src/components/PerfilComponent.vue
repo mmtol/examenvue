e3bb1709151e451cc7af293a62da8b9a6a4b648b
mmtol <mmigueltolosana@gmail.com>
2025-11-18 10:16:17 +0100

    perfil

--- src/components/PerfilComponent.vue
+++ src/components/PerfilComponent.vue
@@ -1,13 +1,42 @@
 <template>
     <div>
         <h1>PERFIL</h1>
+        <div>
+            <h2>ID: {{ perfil.idUsuario }}</h2>
+            <h3>Nombre: {{ perfil.nombre }}</h3>
+            <h3>Email: {{ perfil.email }}</h3>
+            <p>{{ perfil.pass }}</p>
+        </div>
     </div>
 </template>
 
 <script>
+    import ServiceCubos from './../services/serviceCubos';
+    const service = new ServiceCubos();
+
     export default 
     {
-        
+        data()
+        {
+            return{
+                token:localStorage.getItem('token'),
+                perfil:{}
+            }
+        },
+        mounted()
+        {
+            if (!this.token)
+            {
+                this.$router.push("/login");
+            }
+            else
+            {
+                service.getPerfil(this.token).then(result =>
+                {
+                    this.perfil = result;
+                })
+            }
+        }
     }
 </script>
 
